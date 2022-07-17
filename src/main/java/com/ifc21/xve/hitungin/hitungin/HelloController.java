48b2cac6025c471e3ce8bbc773ae4b72ff1c6e0d
Eky Zakariah <ekyzakariah02@gmail.com>
2022-07-17 12:15:57 +0700

    hitungin: Change text and color btn & Add event

--- src/main/java/com/ifc21/xve/hitungin/hitungin/HelloController.java
+++ src/main/java/com/ifc21/xve/hitungin/hitungin/HelloController.java
@@ -2,6 +2,7 @@ package com.ifc21.xve.hitungin.hitungin;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onTargetSelected(MouseEvent e) {
+        System.out.println(e.getTarget());
     }
 }
