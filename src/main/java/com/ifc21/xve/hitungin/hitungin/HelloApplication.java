c06ce1c417f3852abbbc29acc2f83121e3798bd8
Eky Zakariah <ekyzakariah02@gmail.com>
2022-07-17 11:07:58 +0700

    hitungin: Creating Layout and Button

--- src/main/java/com/ifc21/xve/hitungin/hitungin/HelloApplication.java
+++ src/main/java/com/ifc21/xve/hitungin/hitungin/HelloApplication.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator.fxml"));
+        Scene root = new Scene(fxmlLoader.load(), 390, 720);
+        String css = this.getClass().getResource("assets/css/style.css").toExternalForm();
+        root.getStylesheets().add(css);
+        stage.setTitle("Hitungin Calculator");
+        stage.setScene(root);
         stage.show();
     }
 
